10b68730d372f3701ff336d39bc908223cb9324e
Ikemefuna Nwodo <Ikemefuna.nwodo@globalaccelerex.com>
2022-10-28 08:37:43 +0100

    Add cors

--- src/main/java/com/ikem/hngfirsttask/HngFirstTaskApplication.java
+++ src/main/java/com/ikem/hngfirsttask/HngFirstTaskApplication.java
@@ -2,6 +2,10 @@ package com.ikem.hngfirsttask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class HngFirstTaskApplication {
@@ -10,4 +14,16 @@ public class HngFirstTaskApplication {
         SpringApplication.run(HngFirstTaskApplication.class, args);
     }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
--- src/main/java/com/ikem/hngfirsttask/SecurityConfig.java
+++ src/main/java/com/ikem/hngfirsttask/SecurityConfig.java
@@ -11,6 +11,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+        http.cors().and().httpBasic();
     }
 }
